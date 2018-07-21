47fa53886e50133e26c913f3f41591575ccc96b8
CRDesigner <kdu001987@gmail.com>
2018-07-20 22:20:21 -0300

    Site em construção

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>CRDesigner</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121181555-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-121181555-1');
+		</script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<header id="header">
+				<span class="avatar"><img src="images/logo.png" alt="CRDesigner" /></span>
+				<h1><strong>Site em construção, aguarde novidades em breve!</strong></h1>
+				<ul class="icons">
+					<li><a href="https://www.facebook.com/crdesigner.com.br/" target="_blank" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+					<!--<li><a href="https://www.instagram.com/crdesigner.com.br/" target="_blank" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>-->
+					<li><a href="https://api.whatsapp.com/send?phone=5521983475238" target="_blank" class="icon style2 fa-whatsapp"><span class="label">WhatsApp</span></a></li>
+					<li><a href="mailto:contato@crdesigner.com.br" target="_blank" class="icon style2 fa-envelope-o"><span class="label">E-mail</span></a></li>
+				</ul>
+			</header>
+			<footer id="footer">
+				<p>Copyright &copy; 2018 <a href="http://www.crdesigner.com.br" target="_blank">CRDesigner</a>.</p>
+			</footer>
+		</div>
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.poptrox.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/main.js"></script>
+	</body>
+</html>
